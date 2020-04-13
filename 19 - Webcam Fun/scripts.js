27530faa3c15399eb02946eaad081a846ccde026
MMoronto <t2wycemo@gmail.com>
2020-04-13 19:04:27 -0400

    Update scripts.js

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -8,5 +8,9 @@ function getVideo() {
   navigator.mediaDevices.getUserMedia({ video: true, audio: false})
     .then(localMediaStream => {
       console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
     });
 }
+
+getVideo();
