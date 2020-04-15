9becfea93f1ccbf2378fa4b98bc84fefdcb6947c
MMoronto <t2wycemo@gmail.com>
2020-04-15 11:28:19 -0400

    Update scripts.js

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -11,7 +11,27 @@ function getVideo() {
     
       video.srcObject = localMediaStream;
       video.play();
+    })
+    .catch(err => {
+      console.error('OH NO!!!', err);
     });
 }
+
+function paintToCanvas()  {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    // take pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with them
+    // pixels = redEffect(pixels);
+
+    pixels = rgbSplit(pixels);
+  })
+};
 
 getVideo();
