f9b41b98355f47312b88d5b740e1f0396613dc9f
MMoronto <t2wycemo@gmail.com>
2020-05-02 09:30:11 -0400

    js update

--- 29-CountdownTimer/scripts.js
+++ 29-CountdownTimer/scripts.js
@@ -6,4 +6,16 @@ const buttons = document.querySelectorAll('[data-time]');
 function timer(seconds) {
   // clear any existing timers
   clearInterval(countdown);
+  
+  const now = Date.now();
 }
+
+
+  buttons.forEach(button => button.addEventListener('click', startTimer));
+  document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+  });
