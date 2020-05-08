907e00b87190777694e1336f31bafb622a29a479
MMoronto <t2wycemo@gmail.com>
2020-05-08 06:34:34 -0400

    Update js

--- 31-RandomMealGenerator/scripts.js
+++ 31-RandomMealGenerator/scripts.js
@@ -51,3 +51,19 @@ const createMeal = (meal) => {
       `;
       meal_container.newInnerHTML = newInnerHTML;
 }
+
+
+
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+  social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+  social_panel_container.classList.remove('visible')
+});
