b2b20db0476995903f4ffe46c6580586a5d2dc21
MMoronto <t2wycemo@gmail.com>
2020-05-15 10:27:45 -0400

    Update script, RegEx. Add Notes

--- 33-CSV2JSON/scripts.js
+++ 33-CSV2JSON/scripts.js
@@ -16,22 +16,30 @@ function CSVToArray(strData, strDelimiter) {
       "\\r\\n]*))",
       "gi"
   );
-
+  // Create an array to hold our data. Give the array
+  // a default empty first row.
   var arrData =[[]];
-
+  // Create an array to hold our individual pattern
+  // matching groups.
   var arrMatches = null;
-
+  // Keep looping over the regular expression matches
+  // until we can no longer find a match.
   while ((arrMatches = objPattern.exec(strData))) {
+    // Get the delimiter that was found.
     var strMatchedDelimiter = arrMatches[1];
-
+    // Check to see if the given delimiter has a length
+    // (is not the start of string) and if it matches
+    // field delimiter. If id does not, then we know
+    // that this delimiter is a row delimiter.
     if (strMatchedDelimiter.length && strMatchedDelimiter != strDelimiter) {
-
+      // Since we have reached a new row of data,
+      // add an empty row to our data array.
       arrData.push([]);
     }
 
     if (arrMatches[2]) {
 
-      var strMatchedValue = arrMatches[2].replace(new RegExp('""', "g"), '""');
+      var strMatchedValue = arrMatches[2].replace(new RegExp('""', "g"), '"');
     } else {
 
       var strMatchedValue = arrMatches[3];
