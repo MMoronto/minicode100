9e7e28e4af1f0925768b858c7ffc5cf1dcbf8203
MMoronto <t2wycemo@gmail.com>
2020-05-18 14:11:17 -0400

    Update JS

--- 34-JSCalculator/scripts.js
+++ 34-JSCalculator/scripts.js
@@ -16,8 +16,43 @@ TODO:
 
     return document.querySelectorAll(element); // Otherwise, returns a nodelist
   };
-
   // Variables
   var viewer = el("#viewer"), // Calculator screen where result is displayed
-  
-})
+    equals = el("#equals"), // Equal button
+    nums = el(".num"), // List of numbers
+    ops = el(".ops"), // List of operators
+    theNum = "", // Current number
+    oldNum = "", // First number
+    resultNum, // Result
+    operator; // Batman
+
+    // When: Number is clicked. Get the current number selected
+    var setNum = function() {};
+
+    // When: Operator is clicked. Pass number to oldNum and save operator
+    var displayNum = function() {};
+
+    // When: Equals is clicked. Calculate result
+    var displayNum = function() {};
+
+    //When: Clear button is pressed. Clear everything
+    var clearAll = function() {};
+
+    /* The click events */
+
+    // Add click event to numbers
+    for (var i = 0, l = nums.length; i < l; i++) {}
+
+    // Add click event to operators
+    for (var i = 0, l = ops.length; i < l; i++) {}
+
+    // Add click event to equal sign
+    equals.onclick = displayNum;
+
+    // Add click event to clear button
+    el("#clear").onclick = clearAll;
+
+    // Add click event to reset button
+    el("#reset").onclick = function() {};
+    
+}());
