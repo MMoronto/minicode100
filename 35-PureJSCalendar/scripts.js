a8844fa242fa427f00b1c239c56d8b55cc9a8900
MMoronto <t2wycemo@gmail.com>
2020-05-20 17:52:13 -0400

    Update JS

--- 35-PureJSCalendar/scripts.js
+++ 35-PureJSCalendar/scripts.js
@@ -9,11 +9,23 @@ months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", 
 monthAnYear = document.getElementById("monthAndYear");
 showCalendar(currentMonth, currentYear);
 
-function next() {}
+function next() {
+  currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+  currentMonth = (currentMonth + 1) % 12;
+  showCalendar(currentMonth, currentYear);  
+}
 
-function previous() {}
+function previous() {
+  currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+  currentMonth = (currentMonth === 0) ? 11: currentMonth - 1;
+  showCalendar(currentMonth, currentYear);  
+}
 
-function jump() {}
+function jump() {
+  currentYear = parseInt(selectYear.value);
+  currentMonth = parseInt(selectMonth.value);
+  showCalendar(currentMonth, currentYear);  
+}
 
 function showCalendar(month, year) {}
 
