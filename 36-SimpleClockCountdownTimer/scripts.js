f8d5b4782ac9b7924974fd583ada65305af36300
MMoronto <t2wycemo@gmail.com>
2020-05-23 19:06:34 -0400

    Simple Clock/Countdown Timer
    Simple JS and CSS Countdown Timer that provides a continuously decrementing display of the months, days, hours, minutes and seconds to a user entered event.

---
+++ 36-SimpleClockCountdownTimer/scripts.js
@@ -0,0 +1,13 @@
+// load event listeners
+loadEventListeners();
+
+function loadEventListeners() {};
+
+var timeTo = document.getElementsById('time-to').value,
+    date,
+    now = new Date(),
+    newYear = new Date('1.1.2020').getTime(),
+    startTimer = '';
+
+// calculate date, hour, minute and second
+function calcTime(dates) {}
