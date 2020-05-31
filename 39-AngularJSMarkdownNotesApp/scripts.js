e74656f703a83b0ef057a8701b624865b176f5c1
MMoronto <t2wycemo@gmail.com>
2020-05-30 21:43:44 -0400

    Angular JS Markdown Notes App
    Build an App that lets the user create and store your notes for later purpose.

---
+++ 39-AngularJSMarkdownNotesApp/scripts.js
@@ -0,0 +1,35 @@
+(function () {
+  var app = angular.module("notesApp", ["angular-markdown-editable"]);
+
+  app.controller("notesController", function($scope) {
+    // Initial Data
+    $scope.notes = [
+      {
+        createdOn: 1428237500771,
+        edit: false,
+        text:
+          "#Hello, World!\n\nThis is your first Angular Markdown note. You can:\n\n* Click/Focus to edit\n\n* Click off/Blur to save\n\n* Add a new note  by clicking the plus sign above.\n\n* Delete this note\n\nMarkdown compiled using the fantastic [angular-markdown-editable](http://projects.quiver.is/angular-markdown-editable/) directive."
+      }
+    ];
+
+    // Add New Note
+    $scope.addNote = function () {
+      $scope.newNote = {};
+    };
+
+    // Delete Note
+    $scope.delete = function (i) {
+      var r = confirm("Are you sure you want to delete this note?");
+    };
+
+    // Update Note
+    $scope.update = function (i, note) {
+      $scope.notes[i].text = note;
+      $scope.notes[i].edit = false;
+    };
+
+    // End Controller
+  });
+
+  // End Function
+})();
