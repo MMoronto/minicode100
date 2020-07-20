3777a5441135b7d265608c26c6cba9ddeaf7f9e1
MMoronto <t2wycemo@gmail.com>
2020-07-20 18:09:37 -0400

    2020 Mood Calendar
    Recreating Florin Pop's JS Mood calendar

---
+++ 44-MoodCalendar/scripts.js
@@ -0,0 +1,15 @@
+const currentYear = 2020;
+const weekDays = [];
+const months = [];
+const colors = [];
+const defaultColor = ;
+let activeColor = '';
+
+const calendar = document.getElementById('calendar');
+const defaultColor = '#888';
+let activeColor = '';
+
+const calendar = document.getElementById('calendar');
+const moods = document.querySelectorAll('.mood');
+const randomize = document.querySelector('#randomize');
+const clear = document.querySelector('#clear'); 
