093895f557e20b13af803943583a87ecd1f2547e
MMoronto <t2wycemo@gmail.com>
2020-07-20 21:20:41 -0400

    Update JS

--- 44-MoodCalendar/scripts.js
+++ 44-MoodCalendar/scripts.js
@@ -13,3 +13,27 @@ const calendar = document.getElementById('calendar');
 const moods = document.querySelectorAll('.mood');
 const randomize = document.querySelector('#randomize');
 const clear = document.querySelector('#clear'); 
+
+moods.forEach(mood => {});
+
+const getAllDays = year => {};
+
+const dates = getAllDays(currentYear);
+
+let monthsHTML = '';
+
+// Loop over the months and create a div for each month
+months.forEach((month, idx) => {});
+
+calendar.innerHTML = monthsHTML;
+
+// Loop over each day and
+dates.forEach(date => {});
+
+// Add click event to all the .circles
+const circles = document.querySelectorAll('.circle');
+circles.forEach(circle => {
+	circle.addEventListener('click', () => {
+		circle.style.backgroundColor = activeColor;
+	});
+}); 
