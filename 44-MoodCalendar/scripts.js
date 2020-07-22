41c64912ec3e96038f6ebd5534a9e7c05b3a8739
MMoronto <t2wycemo@gmail.com>
2020-07-22 17:15:31 -0400

    Update JS

--- 44-MoodCalendar/scripts.js
+++ 44-MoodCalendar/scripts.js
@@ -1,13 +1,23 @@
 const currentYear = 2020;
-const weekDays = [];
-const months = [];
-const colors = [];
-const defaultColor = ;
-let activeColor = '';
-
-const calendar = document.getElementById('calendar');
+const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const months = [
+  'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'Octomber',
+	'November',
+	'December'
+];
+const colors = ['#2d6b5f', '#72e3a6', '#dff4c7', '#edbf98', '#ea3d36'];
 const defaultColor = '#888';
 let activeColor = '';
+
 
 const calendar = document.getElementById('calendar');
 const moods = document.querySelectorAll('.mood');
