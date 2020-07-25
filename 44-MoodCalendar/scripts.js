bcdb8dcde20f3f0a46d863f9ed9a53e60a4d7bee
MMoronto <t2wycemo@gmail.com>
2020-07-24 21:48:49 -0400

    Update JS

--- 44-MoodCalendar/scripts.js
+++ 44-MoodCalendar/scripts.js
@@ -108,11 +108,30 @@ clear.addEventListener('click', () => {
 	}); 
 });
 
-function getRandomColor() {}
+function getRandomColor() {
+	return colors[Math.floor(Math.random() * 5)];
+}
 
-function createDateEl(date) {}
+function createDateEl(date) {
+  const day = date.getDate();
+	const dateEl = document.createElement('div');
+	dateEl.classList.add('days');
+	dateEl.innerHTML = `<span class="circle">${day}</span>`;
 
-function createEmptySpot() {}
+	return dateEl;
+}
+
+function createEmptySpot() {
+  const emptyEl = document.createElement('div');
+	emptyEl.classList.add('days');
+
+	return emptyEl;
+}
 
 //from stackoverfllow???
-function addDays(date, days) {}
+function addDays(date, days) {
+  var result = new Date(date);
+	result.setDate(result.getDate() + days);
+	return result; 
+}
+
