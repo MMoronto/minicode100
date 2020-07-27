6608710c4a131dbeae2bf24e8c700eb202251d08
MMoronto <t2wycemo@gmail.com>
2020-07-27 17:50:38 -0400

    Update JS

--- 45-DoubleNoiseFlowField/scripts.js
+++ 45-DoubleNoiseFlowField/scripts.js
@@ -56,11 +56,39 @@ class Particle {
 		this.checkEdges();    
   }
 
-  draw() {}
+  draw() {
+    stroke(random() < 0.5 ? purple : 'rgba(255,255,255, 1)');
+		// stroke();
+		strokeWeight(0.5);
+		line(this.prevPos.x, this.prevPos.y, this.pos.x, this.pos.y);
+	}
 
-  checkEdges() {}
+  checkEdges() {
+    if (this.pos.x > width) {
+			this.pos.x = 0;
+			this.prevPos = this.pos.copy();
+		}
+		if (this.pos.x < 0) {
+			this.pos.x = width;
+			this.prevPos = this.pos.copy();
+		}
+		if (this.pos.y > height) {
+			this.pos.y = 0;
+			this.prevPos = this.pos.copy();
+		}
+		if (this.pos.y < 0) {
+			this.pos.y = height;
+			this.prevPos = this.pos.copy();
+		}
+	}
 
-  setForceFromField(field) {}
+  setForceFromField(field) {
+    const x = Math.floor(this.pos.x / size);
+		const y = Math.floor(this.pos.y / size);
+		const idx = y + x * size;
+		const force = field[idx];
+		this.applyForce(force);
+	}
 }
 
 function createField() {}
