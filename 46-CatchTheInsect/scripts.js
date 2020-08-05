64d5c8272133649250db5f046445850768b300a2
MMoronto <t2wycemo@gmail.com>
2020-08-04 20:38:55 -0400

    Update JS

--- 46-CatchTheInsect/scripts.js
+++ 46-CatchTheInsect/scripts.js
@@ -1,2 +1,11 @@
 const start_btn = document.getElementById('start_btn');
 const screens = document.querySelectorAll('.screen');
+const choose_insect_btns = document.querySelectorAll('.choose_insect_btn');
+const game_container = document.querySelector('.game_container');
+const timeEl = document.getElementById('time');
+const scoreEl = document.getElementById('score');
+const annoying_message = document.getElementById('annoying_message');
+let seconds = 0;
+let score = 0;
+let selected_insect = {};
+
