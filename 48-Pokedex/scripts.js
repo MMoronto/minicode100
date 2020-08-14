b80f1c9161be36518fe8580ca85b2a666771dc2e
MMoronto <t2wycemo@gmail.com>
2020-08-13 20:52:40 -0400

    Update JS

--- 48-Pokedex/scripts.js
+++ 48-Pokedex/scripts.js
@@ -1,1 +1,12 @@
-// ADD JS HERE
+const poke_container = document.getElementById('poke_container');
+const pokemons_number = 150;
+const colors = {};
+const main_types = Object.keys(colors);
+
+const fetchPokemons = async () => {};
+
+const getPokemon = async id => {};
+
+function createPokemonCard(pokemon) {}
+
+fetchPokemons();
