14a1b4bc4c5a4fdc192a883c6aa3ebbd812fb8a7
MMoronto <t2wycemo@gmail.com>
2020-08-17 18:02:28 -0400

    Update JS

--- 48-Pokedex/scripts.js
+++ 48-Pokedex/scripts.js
@@ -43,13 +43,19 @@ function createPokemonCard(pokemon) {
 	pokemonEl.style.backgroundColor = color;
 	
 	const pokeInnerHTML = `
-		<div>
-			
+		<div class="img-container">
+			<img src="https://pokeres.bastionbot.org/images/pokemon/${
+				pokemon.id
+			}.png" alt="${name}" />			
 		</div>
-		<div>
-			
+		<div class="info">
+			<span class="number">#${pokemon.id
+				.toString()
+				.padStart(3, '0')}</span>
+			<h3 class="name">${name}</h3>
+			<small class="type">Type: <span>${type}</span></small>
 		</div>
-  `;
+  	`;
 	
 pokemon.innerHTML = pokeInnerHTML;
 	
