9da0ae10af88efaa624910dbcc40a78d63ec1712
MMoronto <t2wycemo@gmail.com>
2020-10-14 22:41:44 -0400

    Update JS

--- 50-LiveTwitterFeed/scripts.js
+++ 50-LiveTwitterFeed/scripts.js
@@ -1,3 +1,14 @@
 const tweets_container = document.getElementById('tweets_container');
 const dummyTweets = [];
 const tweets = [];
+
+const getTweets = () => {}
+
+const createTweet = (tweet) => {}
+
+const updateTimes = () => {}
+
+function tweetsInterval() {}
+
+getTweets();
+setInterval(tweetsInterval, 5000);
