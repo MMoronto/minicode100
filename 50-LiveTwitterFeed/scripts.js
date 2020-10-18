379d46da0bacfeb453c90ccc7eb8c30c9594a98f
MMoronto <t2wycemo@gmail.com>
2020-10-17 23:59:20 -0400

    Update JS

--- 50-LiveTwitterFeed/scripts.js
+++ 50-LiveTwitterFeed/scripts.js
@@ -8,7 +8,14 @@ const dummyTweets = [
     created: Date.now(),
     text: 'Happy Mothers Day to all the amazing mothers out there! The world is truly a better place because of your... http://fb.me/6vncfjh3d'    
   },
-  {},
+  {
+		userImage:
+			'https://pbs.twimg.com/profile_images/css-1dbjc4n r-1twgtwe r-sdzlij r-rs99b7 r-1p0dtai r-1mi75qu r-1d2f490 r-1ny4l3l r-u8s1d r-zchlnj r-ipm5af r-o7ynqc r-6416eg',
+    userName: 'Mobola Moronto',
+    userScreen: '@MMoronto',
+		created: Date.now() - 50000,
+		text: 'Twitter API limit reached. 😭'    
+  },
   {},
   {},
 ];
