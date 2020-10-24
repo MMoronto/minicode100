237797648d1a6c7227101e165d01eb6b000be026
MMoronto <t2wycemo@gmail.com>
2020-10-24 09:30:36 -0400

    Update JS

--- 52-DogPicker/scripts.js
+++ 52-DogPicker/scripts.js
@@ -1,67 +1,71 @@
 class App extends React.Component {
-  state = {
-    dogs: [],
-    database: undefined,
-    idx1: undefined,
-    idx2: undefined
-  };
+    state = {
+      dogs: [],
+      database: undefined,
+      idx1: undefined,
+      idx2: undefined
+    };
 
-  componentDidMount = () => {
-    const database = firebase.database().ref('/dogs');
+    componentDidMount = () => {
+      const database = firebase.database().ref('/dogs');
 
-    this.setState(
-    {
-      database
-    },
-    () => {
+      this.setState(
+      {
+        database
+      },
+      () => {
+        this.getDogs();
+      }
+    );
+
+    setTimeout(() => {
+      this.getRandomDogs();
+    }, 2000);  
+    };
+
+    getDogs = () => {
+    const {database} = this.state;
+
+    database.on('value', snapshot => {
+      const dogs = [];
+      const dogsObj = snapshot.val();
+      Object.keys(dogsObj).forEach(key => {
+        dogs.push(dogObj[key]);
+      });
+
+      this.setState({
+        dogs
+      });
+    });
+    };
+
+    favoriteDog = id => {
+      const {databse, dogs } = this.state;
+      const dog = dogs.find(d => d.id == id);
+
+      // increase the likes??
+      database.child(id).update({ likes: dog.likes + 1 });
+      this.getRandomDogs();
       this.getDogs();
-    }
-  );
+    };
 
-  setTimeout(() => {
-    this.getRandomDogs();
-  }, 2000);  
-};
+    getRandomDogs = () => {
+      const { dogs } = this.state;
 
-getDogs = () => {
-  const {database} = this.state;
+      if (!dogs) {
+        return;
+      }
 
-  database.on('value', snapshot => {
-    const dogs = [];
-    const dogsObj = snapshot.val();
-    Object.keys(dogsObj).forEach(key => {
-      dogs.push(dogObj[key]);
-    });
+      const idx1 = Math.floor(Math.random() * dogs.length);
+      const idx2 = Math.floor(Math.random() * dogs.length);
 
-    this.setState({
-      dogs
-    });
-  });
-};
+      this.setState({ idx1, idx2});
+    };
 
-favoriteDog = id => {
-    const {databse, dogs } = this.state;
-    const dog = dogs.find(d => d.id == id);
+    getMedal = idx => {};
 
-    // increase the likes??
-    database.child(id).update({ likes: dog.likes + 1 });
-    this.getRandomDogs();
-    this.getDogs();
-};
-
-getRandomDogs = () => {
-    const { dogs } = this.state;
-
-    if (!dogs) {
-      return;
-    }
-
-    const idx1 = Math.floor(Math.random() * dogs.length);
-    const idx2 = Math.floor(Math.random() * dogs.length);
-
-    this.setState({ idx1, idx2});
-};
+    render() {}
 
 }
 
-
+ReactDOM.render(<App />, document.getElementById('app'));
