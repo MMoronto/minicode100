b94c5991ba76041d0434271463c7e0a056ffe225
MMoronto <t2wycemo@gmail.com>
2020-10-25 12:07:58 -0400

    Update JS

--- 53-BackgroundPatternGenerator/scripts.js
+++ 53-BackgroundPatternGenerator/scripts.js
@@ -1,1 +1,20 @@
-// ADD JS HERE
+const container = document.getElementById('container');
+const radiusCB = document.querySelectorAll('.radius input');
+const itemsEl = document.getElementById('items');
+const colorsEl = document.getElementById('colors');
+
+const colorsObj = {}
+
+let colors = [...colorsObj];
+let items = 15;
+let radiusArr = ['100%', '0%', '0%', '0%'];
+
+radiusCB.forEach((checkbox, idx) => {});
+
+itemsEl.addEventListener('change', (e) => {});
+
+colorsEl.addEventListener('change', (e) => {});
+
+function createTitles() {}
+
+createTitles();
