37a16712ceefbca6e3cfcaab43171529cb792d62
MMoronto <t2wycemo@gmail.com>
2020-10-30 07:09:38 -0400

    Update JS

--- 57-TestimonialDesign/scripts.js
+++ 57-TestimonialDesign/scripts.js
@@ -1,4 +1,15 @@
 const authorsEl = document.querySelectorAll('.author');
 const container = document.querySelector('.testimonials-container');
+const nameEl = document.querySelector('.name');
+const textEl = document.querySelector('.text');
 
+const testimonials = [{},{
+    text: 'I have worked with him on several occasions. He is very attentive to details and exceeds expectations consistently.',
+    name: 'Kedar Shah',
+    color: 'rgba(250, 130, 49,1.0)'
+},{},{
+    text: 'Morakinyo has been a very valuable resource for my organisation. He is very knowledgeble and gets things done quickly and with suerior quality',
+    name: 'Barry Weitcz',
+    color: 'rgba(165, 94, 234,1.0)'  
+},{}];
 
