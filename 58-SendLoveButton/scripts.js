2251b61460d2b1362f1e1e4abfe90a9e4ad78bd0
MMoronto <t2wycemo@gmail.com>
2020-11-01 16:41:54 -0500

    Update JS

--- 58-SendLoveButton/scripts.js
+++ 58-SendLoveButton/scripts.js
@@ -1,5 +1,9 @@
 const love_btns = document.querySelectorAll('.love');
 
-love_btns.forEach(love_btn => {})
+love_btns.forEach(love_btn => {
+    love_btn.addEventListener('mousedown', (e) => {});
+
+    love_btn.addEventListener('mouseup', (e) => {});
+})
 
 function createHearts(container) {}
