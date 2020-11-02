3e5c8adb0ed9f847606076488568a8797897ed5f
MMoronto <t2wycemo@gmail.com>
2020-11-02 00:44:46 -0500

    Update JS

--- 58-SendLoveButton/scripts.js
+++ 58-SendLoveButton/scripts.js
@@ -3,7 +3,7 @@ const love_btns = document.querySelectorAll('.love');
 love_btns.forEach(love_btn => {
     love_btn.addEventListener('mousedown', (e) => {
         love_btn.style.background = '#fff';
-        love_btn.style.color = '#7273F';
+        love_btn.style.color = '#E7273F';
         love_btn.querySelector('.text').innerHTML = '<span class="grey-text">Sent to:</span> MOBOLA';
 
         createHearts(love_btn.querySelector('.icon-container'));
