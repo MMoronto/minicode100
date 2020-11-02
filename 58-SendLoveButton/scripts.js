81c0a1d3848b3070aeafe7ac0240798955cae52b
MMoronto <t2wycemo@gmail.com>
2020-11-02 00:25:10 -0500

    Update JS

--- 58-SendLoveButton/scripts.js
+++ 58-SendLoveButton/scripts.js
@@ -18,5 +18,7 @@ love_btns.forEach(love_btn => {
 
 function createHearts(container) {
     // create 7 hearts
-    for(let i=0; i<5; i++) {}
+    for(let i=0; i<5; i++) {
+        setTimeout(() => {}, i * 100)
+    }
 }
