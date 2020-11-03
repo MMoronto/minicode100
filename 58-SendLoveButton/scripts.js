252ee6f3cdf055b279cb34e8ddb4b78acbdc995b
MMoronto <t2wycemo@gmail.com>
2020-11-03 14:49:29 -0500

    Update JS

--- 58-SendLoveButton/scripts.js
+++ 58-SendLoveButton/scripts.js
@@ -12,13 +12,13 @@ love_btns.forEach(love_btn => {
     love_btn.addEventListener('mouseup', (e) => {
         love_btn.style.background = '#E7273F';
         love_btn.style.color = '#fff';
-        love_btn.querySelector('.text').innerHTML = 'THANK YOU! <i class="fas fa-redo"></i>'
+        love_btn.querySelector('.text').innerHTML = 'THANK YOU! <i class="fas fa-redo"></i>';
     });
 })
 
 function createHearts(container) {
     // create 7 hearts
-    for(let i=0; i<5; i++) {
+    for(let i=0; i<15; i++) {
         setTimeout(() => {
             const heart = document.createElement('span');
             heart.classList.add('heart');
