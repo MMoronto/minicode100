02eed7054500a34ed2a9ddd717b15bc450fa68cb
MMoronto <t2wycemo@gmail.com>
2020-11-04 08:30:28 -0500

    Update JS

--- 57-TestimonialDesign/scripts.js
+++ 57-TestimonialDesign/scripts.js
@@ -4,37 +4,25 @@ const nameEl = document.querySelector('.name');
 const textEl = document.querySelector('.text');
 
 const testimonials = [{     
-  text: 'I have worked with him on several
-occasions. He is very attentive to details and exceeds expectations
-consistently.',     
+  text: 'I have worked with him on several occasions. He is very attentive to details and exceeds expectations consistently.',     
   name: 'Kedar Shah',     
   color: 'rgba(235, 59, 90,1.0)'
 },{     
-  text: 'An old and trusted collegue recommended Morakinyo\'s services.
-I have worked with him since and I have never been dissapointed. I can\'t
-recommend him enough.'     
+  text: 'An old and trusted collegue recommended Morakinyo\'s services. I have worked with him since and I have never been dissapointed. I can\'t recommend him enough.',     
   name: 'Rajar Kapoor',     
-  color: 'rgba(250, 130,
-49,1.0)'  
+  color: 'rgba(250, 130,49,1.0)'  
 },{     
-  text: 'Professional, very knowlegeable and dependable. I
-highly recommend Morakinyo.'     
+  text: 'Professional, very knowlegeable and dependable. I highly recommend Morakinyo.',     
   name: 'Kelly Kavanaugh',     
-  color: 'rgba(75,
-123, 236,1.0)'   
+  color: 'rgba(75,123, 236,1.0)'   
 },{     
-  text: 'Morakinyo has helped my company get through
-several sticky situations with very tight deadlines. I am so glad he is an
-available resource for our development team.',     
+  text: 'Morakinyo has helped my company get through several sticky situations with very tight deadlines. I am so glad he is an available resource for our development team.',     
   name: 'Shelly Lambert',
   color: 'rgba(165, 94, 234,1.0)'   
 },{     
-  text: 'Morakinyo has been a very
-valuable resource for my organisation. He is very knowledgeble and gets things
-done quickly and with suerior quality',     
+  text: 'Morakinyo has been a very valuable resource for my organisation. He is very knowledgeble and gets things done quickly and with suerior quality',     
   name: 'Barry Weitcz',     
-  color:
-'rgba(32, 191, 107,1.0)'    
+  color:'rgba(32, 191, 107,1.0)'    
 }];
 
 addTestimonial(0);
