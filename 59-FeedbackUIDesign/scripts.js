54520533eacb820d986d93a466dc5ff2123c0168
MMoronto <t2wycemo@gmail.com>
2020-11-08 13:09:48 -0500

    Update JS

--- 59-FeedbackUIDesign/scripts.js
+++ 59-FeedbackUIDesign/scripts.js
@@ -2,7 +2,13 @@ const ratingsEl = document.getElelementById('.rating');
 const sendBtn = document.querySelector('#send');
 const panel = document.querySelector('#panel');
 
-ratingsEl.forEach(el => {});
+ratingsEl.forEach(el => {
+    el.addEventListener('click', () => {
+        ratingsEL.forEach(innerEl => {});
+
+        el.classList.add('active');
+    });
+});
 
 sendBtn.addEventListener('click', () => {});
 
