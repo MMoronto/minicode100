e027ec34b98158a877378f16c167031da0bbe3e8
MMoronto <t2wycemo@gmail.com>
2020-11-15 18:24:23 -0500

    Update JS

--- 61-BakgroundAnimation/scripts.js
+++ 61-BakgroundAnimation/scripts.js
@@ -1,2 +1,7 @@
-const app = document.getElementById'app');
+const app = document.getElementById('app');
+const animation = document.getElementById('animation');
+const BLOCKS_NR = 20;
+const blocks = [];
+const b_width = Math.ceil(window.innerWidth / BLOCKS_NR);
+const b_height = Math.ceil(window.innerHeight / BLOCKS_NR);
 
