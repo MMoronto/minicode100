c389213012d543ff7510d575686f1f306e6b1359
MMoronto <t2wycemo@gmail.com>
2020-11-16 11:49:02 -0500

    Update JS

--- 62-HoverBoard/scripts.js
+++ 62-HoverBoard/scripts.js
@@ -17,7 +17,11 @@ for(let i=0; i<SQUARES_NR; i++) {
     container.appendChild(square);
 }
 
-function setColorToEl(element) {}
+function setColorToEl(element) {
+    const color = getRandomColor();
+    element.style.background = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
 
 function removeColorFromEl(element) {}
 
