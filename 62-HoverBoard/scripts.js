b964fb8866bc40cefc9cfcb4aca1ca5e8ab9089e
MMoronto <t2wycemo@gmail.com>
2020-11-16 11:46:12 -0500

    Update JS

--- 62-HoverBoard/scripts.js
+++ 62-HoverBoard/scripts.js
@@ -6,9 +6,13 @@ for(let i=0; i<SQUARES_NR; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
 
-    square.addEventLister('mouseover', () => {});
+    square.addEventLister('mouseover', () => {
+        setColorToEl(square);
+    });
 
-    square.addEventLister('mouseout', () => {});
+    square.addEventLister('mouseout', () => {
+        removeColorFromEl(square);
+    });
 
     container.appendChild(square);
 }
