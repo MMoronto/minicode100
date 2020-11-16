8b6f6bbecccef82c0b4c2f1d378be56660982df9
MMoronto <t2wycemo@gmail.com>
2020-11-16 11:36:51 -0500

    Update JS

--- 62-HoverBoard/scripts.js
+++ 62-HoverBoard/scripts.js
@@ -2,3 +2,10 @@ const container = document.getElementById('container');
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71' ];
 const SQUARES_NR = 500;
 
+for(let i=0; i<SQUARES_NR; i++) {}
+
+function setColorToEl(element) {}
+
+function removeColorFromEl(element) {}
+
+function getRandomColor() {}
