f180fc8b2405c8964c063407c66107eeac3497d3
MMoronto <t2wycemo@gmail.com>
2020-11-23 11:05:23 -0500

    Update JS

--- 65-LineThroughEffect/scripts.js
+++ 65-LineThroughEffect/scripts.js
@@ -1,12 +1,15 @@
-const progressDone = document.querySelectorAll('.progress-done');
+const text = document.getElementById('text');
 const textArr = text.innerText.split('');
 
 const newEl = document.createElement('h1');
 newEl.innerHTML = `
     ${textArr.map(letter => `<span class="letter" style="${randomVisibility()}">${letter}</span>`).join('')}
 `;
-newEl.classicList.add('overLay');
+newEl.classList.add('overLay');
 
 document.body.appendChild(newEl);
 
-function randomVisibility() {}
+function randomVisibility() {
+    return Math.random() < 0.5 ? 'visibility: hidden' : 'visibility: visible';
+}
+
