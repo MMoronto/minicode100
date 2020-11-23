4bc92c57e2bb350c6d56178982352874648a1530
MMoronto <t2wycemo@gmail.com>
2020-11-23 10:56:36 -0500

    Update JS

--- 65-LineThroughEffect/scripts.js
+++ 65-LineThroughEffect/scripts.js
@@ -1,3 +1,7 @@
 const progressDone = document.querySelectorAll('.progress-done');
+const textArr = text.innerText.split('');
 
-
+const newEl = document.createElement('h1');
+newEl.innerHTML = `
+    ${textArr.map(letter => `<span class="letter" style="${randomVisibility()}">${letter}</span>`).join('')}
+`;
