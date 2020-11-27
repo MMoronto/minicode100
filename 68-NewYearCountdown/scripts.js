852ee5be8bdb0c36715b8003fe6249e1e9c52695
MMoronto <t2wycemo@gmail.com>
2020-11-27 15:30:48 -0500

    Update JavaScripts

--- 68-NewYearCountdown/scripts.js
+++ 68-NewYearCountdown/scripts.js
@@ -10,6 +10,17 @@ setInterval(updateCountdown, 1000)
 setInterval(createSnowFlake, 50)
 
 
-function updateCountdown() {}
+function updateCountdown() {
+    const startTime = new Date();
+    const diff = endTime - startTime;
+    const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const minutes = Math.floor(diff / 1000 / 60) % 60;
+    const seconds = Math.floor(diff / 1000) % 60;
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours < 10 ? '0'+hours : hours;
+    minutesEl.innerHTML = minutes < 10 ? '0'+minutes : minutes;
+    secondsEl.innerHTML = seconds < 10 ? '0'+seconds : seconds;    
+}
 
 function createSnowFlake() {}
