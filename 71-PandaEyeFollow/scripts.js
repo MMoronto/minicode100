fac1f545c8d3dfda062f3d558d130320fb832dbc
MMoronto <t2wycemo@gmail.com>
2020-12-06 00:16:46 -0500

    Update JS

--- 71-PandaEyeFollow/scripts.js
+++ 71-PandaEyeFollow/scripts.js
@@ -1,6 +1,8 @@
 const eyes = document.querySelectorAll('.eye-roll');
 
-window.addEventListener('mousemove', (e) => {});
+window.addEventListener('mousemove', (e) => {
+  eyes.forEach(eye => {});
+});
 
 
 
