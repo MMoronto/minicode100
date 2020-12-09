9f4dd9bcf38d691b67d40628868a958e6204f6db
MMoronto <t2wycemo@gmail.com>
2020-12-09 17:48:32 -0500

    Update JS

--- 72-FindIt/scripts.js
+++ 72-FindIt/scripts.js
@@ -1,5 +1,8 @@
 const width= window.innerWidth;
-
+const width= window.innerWidth;
+const height = window.innerHeight;
+const rowLength = Math.floor(height / 30);
+const colLength = Math.floor(width / 30) - 1;
 
 
 
