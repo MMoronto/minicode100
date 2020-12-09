91cca3faa2a771b5ad85fc97a77c85c579f07dfb
MMoronto <t2wycemo@gmail.com>
2020-12-09 17:53:22 -0500

    Update JS

--- 72-FindIt/scripts.js
+++ 72-FindIt/scripts.js
@@ -1,10 +1,15 @@
 const width= window.innerWidth;
-const width= window.innerWidth;
 const height = window.innerHeight;
 const rowLength = Math.floor(height / 30);
 const colLength = Math.floor(width / 30) - 1;
 
+console.log(width, height, rowLength, colLength);
 
+for(let i=0; i<rowLength; i++) {
+
+}
+
+function isTheWinner() {}
 
 
 
