302ee5fbfd71eacc756f31190f78f11b7dc3f8c0
MMoronto <t2wycemo@gmail.com>
2020-12-12 00:20:59 -0500

    Update JS

--- 73-Particles/scripts.js
+++ 73-Particles/scripts.js
@@ -38,9 +38,13 @@ class Particle {
   }
 
   edges() {
-    if(this.pos.x < 0 || this.pos.x > width) {}
+    if(this.pos.x < 0 || this.pos.x > width) {
+      this.vel.x *= -1;
+    }
 
-    if(this.pos.y < 0 || this.pos.y > height) {}
+    if(this.pos.y < 0 || this.pos.y > height) {
+      this.vel.y *= -1;
+    }
   }
 
   checkParticles(particles) {}
