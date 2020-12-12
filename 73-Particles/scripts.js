5b842df427dce0b051538c641c7bfd2661fe72e4
MMoronto <t2wycemo@gmail.com>
2020-12-11 22:36:02 -0500

    Update JS

--- 73-Particles/scripts.js
+++ 73-Particles/scripts.js
@@ -3,7 +3,9 @@ function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
 
   const particlesLength = Math.min(Math.floor(window.innerWidth / 10), 100);
-  for(let i=0; i<particlesLength; i++) {}
+  for(let i=0; i<particlesLength; i++) {
+    particles.push(new Particle());
+  }
 }
 
 function draw() {}
