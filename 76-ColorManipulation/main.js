d94bf8ac656f3f2c3c4be1263af2a6c490028f4c
MMoronto <t2wycemo@gmail.com>
2020-12-29 08:13:37 -0500

    Update JavaScript

--- 76-ColorManipulation/main.js
+++ 76-ColorManipulation/main.js
@@ -4,6 +4,19 @@ import Map from 'ol/Map';
 import View from 'ol/View';
 import {Raster as RasterSource, Stamen} from 'ol/source';
 
+/**
+ * Color manipulation functions below are adapted from
+ * https://github.com/d3/d3-color.
+ */
+ var Xn = 0.95047;
+ var Yn = 1;
+ var Zn = 1.08883;
+ var t0 = 4 / 29;
+ var t1 = 6 / 29;
+ var t2 = 3 * t1 * t1;
+ var t3 = t1 * t1 * t1;
+ var twoPi = 2 * Math.PI;
+
 
 var map = new Map({
   layers: [
