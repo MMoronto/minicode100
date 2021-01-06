4393e3c5ba96055819195a9f3285a652c305351f
MMoronto <t2wycemo@gmail.com>
2021-01-06 09:32:17 -0500

    Map Interactions
    Create Map with Interaction Capabilities in OpenLayers. HTML, CSS, JavaScript & OPenLayers.

---
+++ 78-MapInteractions/main.js
@@ -0,0 +1,36 @@
+window.onload = init;
+
+function init(){
+  const map = new ol.Map({
+      view: new ol.View({
+        center: [-110.0, 40.0],
+        zoom: 4,
+        maxZoom: 6,
+        minZoom: 2,
+        rotation: 0.5
+      }),
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })   
+      ],      
+      target: 'js-map'
+  })
+
+  const popupContainerElement = document.getElementById('popup-coordinates')
+  const popup = new ol.Overlay({
+    element: popupContainerElement,
+    positioning: 'center-right'
+  })
+
+  map.addOverlay(popup);
+
+  map.on('click', function(e){
+    const clickedCoordinate = e.coordinate;
+    popup.setPosition(undefined);
+    popup.setPosition(clickedCoordinate);
+    popupContainerElement.innerHTML = clickedCoordinate;
+  })
+}
+
+
