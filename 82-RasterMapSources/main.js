4c5413a6ed1d66cbd359f9bba328b24c2276d668
MMoronto <t2wycemo@gmail.com>
2021-01-09 15:17:41 -0500

    Raster Map Sources
    Create Tiled Raster Layers using base maps from popular raster sources like Bingmaps, CartoDB and Stamen Raster in OpenLayers. JavaScript, CSS and HTML.

---
+++ 82-RasterMapSources/main.js
@@ -0,0 +1,45 @@
+window.onload = init;
+
+function init(){
+
+  const map = new ol.Map({
+      view: new ol.View({
+        center: [-110.0, 40.0],
+        zoom: 4,
+        // extent: [283239.2900911544, 432768.68455942767, 1647964.7192883273, 1590293.5028198026]
+      }),
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM(),
+          zIndex: 0,
+          visible: true,
+          extent: [283239.2900911544, 432768.68455942767, 1647964.7192883273, 1590293.5028198026],
+          opacity: 0.5
+        })   
+      ],      
+      target: 'js-map',
+  })
+
+  // Layer Group
+  const layerGroup = new ol.layer.Group({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM({
+          url: 'https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+        }),
+        zIndex: 0,
+        visible: false,
+        extent: [283239.2900911544, 432768.68455942767, 1647964.7192883273, 1590293.5028198026],
+        opacity: 0
+      }),
+
+    ]
+  })
+  map.addLayer(layerGroup);
+
+  map.on('click', function(e){
+    console.log(e.coordinate);
+  })
+}
+
+
