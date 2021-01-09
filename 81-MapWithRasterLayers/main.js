c8cdd34bf23ad240d31d66e759f8a2ee67bf6986
MMoronto <t2wycemo@gmail.com>
2021-01-09 07:41:02 -0500

    Update JavaScript
    Add layer extent, view extent and opacity to raster map layers

--- 81-MapWithRasterLayers/main.js
+++ 81-MapWithRasterLayers/main.js
@@ -6,10 +6,15 @@ function init(){
       view: new ol.View({
         center: [-110.0, 40.0],
         zoom: 4,
+        // extent: [283239.2900911544, 432768.68455942767, 1647964.7192883273, 1590293.5028198026]
       }),
       layers: [
         new ol.layer.Tile({
-          source: new ol.source.OSM()
+          source: new ol.source.OSM(),
+          zIndex: 1,
+          visible: true,
+          extent: [283239.2900911544, 432768.68455942767, 1647964.7192883273, 1590293.5028198026],
+          opacity: 0.5
         })   
       ],      
       target: 'js-map',
@@ -23,11 +28,17 @@ function init(){
           url: 'https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
         }),
         zIndex: 0,
-        visible: false
+        visible: true,
+        extent: [283239.2900911544, 432768.68455942767, 1647964.7192883273, 1590293.5028198026],
+        opacity: 0.5
       })
     ]
   })
   map.addLayer(layerGroup);
+
+  map.on('click', function(e){
+    console.log(e.coordinate);
+  })
 }
 
 
